1a1f45659cb45c64146aa17519381d5f863b3b46
javajdash <156153145+javajdash@users.noreply.github.com>
2024-01-30 19:37:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+// Write a program that displays Welcome to Java five times.
+
+class Main {
+  public static void main(String[] args) {
+    // For loop that prints out "Welcome to Java" five times
+    for (int i=0; i<5; i++) {
+      // Print "Welcome to Java"
+      System.out.println("Welcome to Java");
+    }
+  }
+}
